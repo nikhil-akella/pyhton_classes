eb23665f86491a9bf4b9df1a8cf7cc16a4b2eff4
nikhil-akella <raviteja@kriyative-edge.com>
2019-01-13 16:23:32 +0530

    my first comment

---
+++ week_02/first.py
@@ -0,0 +1,138 @@
+print("conversion from integer to other")
+a = 25
+print (a)
+print(type(a))
+
+b = float(a)
+print(b)
+print(type(b))
+
+
+c = str(a)
+print(c)
+print(type(c))
+
+d = bool(a)
+print(d)
+print(type(d))
+
+#case 2
+
+e = 0
+print(e)
+print(type(e))
+
+f = float(e)
+print(f)
+print(type(f))
+
+g = str(e)
+print(g)
+print(type(g))
+
+h = bool(e)
+print(h)
+print(type(h))
+
+#float to other
+
+i = 2.0
+print(i)
+print(type(i))
+
+j = int(i)
+print (j)
+print(type(j))
+
+k = str(i)
+print(k)
+print(type(k))
+
+l = bool(i)
+print(l)
+print(type(l))
+#case 2
+m = 4.5
+print(m)
+print(type(m))
+
+n = int(m)
+print (n)
+print(type(n))
+
+o = str(m)
+print(o)
+print(type(o))
+
+p = bool(m)
+print(p)
+print(type(p))
+
+#string to other
+
+q = "nice"
+print(q)
+print(type(q))
+
+r = int(q)
+print(r)
+print(type(r))
+
+s = float(q)
+print(q)
+print(type(q))
+
+t = bool(q)
+print(t)
+print(type(q))
+
+#case 2
+u = "iron man"
+print(u)
+print(type(u))
+
+v = int(u)
+print(v)
+print(type(v))
+
+w = float(u)
+print(w)
+print(type(w))
+
+x = bool(u)
+print(x)
+print(type(x))
+
+#boolean to other
+
+A = True
+print(A)
+
+B = str(A)
+print(B)
+print(type(B))
+
+C = int(A)
+print(C)
+print(type(C))
+
+D = float(A)
+print(D)
+print(type(D))
+
+#case 2
+
+E = False
+print(E)
+
+F = str(E)
+print(F)
+print(type(F))
+
+G = int(E)
+print(G)
+print(type(G))
+
+H = float(E)
+print(H)
+print(type(H))
---
+++ week_3/variables_keywords.py
@@ -0,0 +1,40 @@
+print("we are in week three")
+
+
+a = 5
+print(a)
+print(type(a))
+
+
+b = "5a"
+print(b)
+print(type(b))
+
+c = True
+print(c)
+print(type(c))
+
+d = 0.67
+print(d)
+print(type(d))
+
+
+clasS = 10
+print(clasS)
+
+i = 555
+print(i)
+print(float(i))
+
+dec = 4.5
+print(dec)
+
+i_dec = int(5.5)
+print(i_dec)
+
+s = "45454666"
+print((s))
+print(type(s))
+i_dec = int(s)
+print(i_dec)
+print(type(i_dec))
