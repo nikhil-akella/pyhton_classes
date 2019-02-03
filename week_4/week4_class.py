bb45b1f3296c6047c73dead93854baa1f7cdbb44
nikhil-akella <raviteja@kriyative-edge.com>
2019-02-03 18:05:56 +0530

    commit

---
+++ week_4/week4_class.py
@@ -0,0 +1,55 @@
+a = 10
+b = 3
+c = a + b
+print("add")
+print(c) #addition
+d = a - b
+print("sub")
+print(d)#sub
+e = a * b
+print("mult")
+print(e)#mult
+f = a/b
+print("div")
+print(f)#div
+g = a % b
+print("mod")
+print(g)#mod
+h = a ** b
+print("expo")
+print(h)#expo
+
+
+i = 23
+x = 45
+z = i ** x
+print(z)
+
+A = 10
+B = 3
+print( A > B)
+print( A < B)
+print( A ==B)
+print(A != B)
+print(A <= B)
+print(A >= B)
+
+e = 10
+f = 3
+if a >b:
+    print("e is greater than f")
+else:
+    print("e is greater than f")
+
+#task
+
+a = int(input("type a number {a} = "))
+b = int(input(" type another number{b} =  "))
+
+if a>b:
+    print("a is greater than b")
+else:
+    print("b is greater than a")
+
+
+
